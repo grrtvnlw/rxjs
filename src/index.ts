6303f77b3caebb8fa7fbfaa5b045e5fe211fffca
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2023-06-13 10:09:26 -0400

    add interval

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,23 @@ observable$.subscribe({
   complete: () => console.log('Completed'),
 });
 console.log('After subscribe');
+
+const interval$ = new Observable<number>((subscriber) => {
+  let counter = 1;
+
+  const intervalId = setInterval(() => {
+    console.log('Emitted', counter);
+    subscriber.next(counter++);
+  }, 2000);
+
+  return () => {
+    clearInterval(intervalId);
+  };
+});
+
+const subscription = interval$.subscribe((value) => console.log(value));
+
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 7000);
