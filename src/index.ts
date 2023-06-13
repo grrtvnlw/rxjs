b670e713cbd46124c3951bcb8569db7b902e13cd
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2023-06-13 10:00:59 -0400

    add error and complete notification handlers

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,23 @@ const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben');
-  setTimeout(() => subscriber.next('Charlie'), 2000);
+  setTimeout(() => {
+    subscriber.next('Charlie');
+  }, 2000);
+  setTimeout(() => {
+    subscriber.error(new Error('Failure'));
+  }, 4000);
+  subscriber.complete();
+
+  return () => {
+    console.log('Teardown');
+  };
 });
 
 console.log('Before subscribe');
-observable$.subscribe((value) => console.log(value));
+observable$.subscribe({
+  next: (value) => console.log(value),
+  error: (error) => console.log(error.message),
+  complete: () => console.log('Completed'),
+});
 console.log('After subscribe');
